04269d98febf17c0fc0ae12803cb78c908518e9d
Heisenberg <80799130+Li-colonel@users.noreply.github.com>
2021-05-06 08:08:07 +0800

    Update MiaoMiao_test.ino

--- MiaoMiao_test.ino
+++ MiaoMiao_test.ino
@@ -1,8 +1,6 @@
 #include "myWebserver.h"
 #include "Pulsesensor.h"
 
-#define ssid "miaomiao"     //这里改成你的设备当前环境下接入点名字
-#define password "12345678" //这里改成你的设备当前环境下接入点密码
 
 Pulsesensor pulseSensor;
 void setup()
